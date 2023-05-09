837c603a075d497dcb62d9033aa62839869f1f3d
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-08 20:51:18 -0700

    troubleshooting routes and typos

--- models/Product.js
+++ models/Product.js
@@ -35,7 +35,7 @@ Product.init(
         isNumeric: true
       }
     },
-    categroy_id: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,12 +8,37 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{
+        model: Product,
+        through: ProductTag,
+        as: 'productTag_products'
+      }],
 
     });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
+  }
+
+});
+
+
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        through: ProductTag,
+        as: 'productTag_products'
+      }],
+
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 
 });
@@ -25,21 +50,6 @@ router.post('/', async (req, res) => {
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
-  }
-
-});
-
-router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
- 
-    });
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
   }
 
 });
